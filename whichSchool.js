4a90c26434fc912fd5d6c3a683727528e034b28b
Zhaosanyin <zhaosan.yin@gmail.com>
2021-07-10 03:57:24 +0000

    logical conditionwhichSchool to go

---
+++ whichSchool.js
@@ -0,0 +1,16 @@
+// function definition first
+const whichSchool = function (ageWhich) {
+    if (ageWhich <13 ) {
+        console.log( "Elementary School");
+    } else if ( 13 <= ageWhich <= 18 ) {
+        console.log ("Secondary School");
+    } else {
+        console.log ("Lighthouse Labs");
+    }
+}
+console.log("I am 35. Which school should I go to?");
+console.log(whichSchool(35));
+console.log("I am 8. Which school should I go to?");
+console.log(whichSchool(8));
+console.log("I am 14. Which school should I go to?");
+console.log(whichSchool(14));
