7604b359c01929c974cda6cdc5d5ba48361954df
Zhaosanyin <zhaosan.yin@gmail.com>
2021-07-12 00:32:21 +0000

    loopy homework2

--- loopy.js
+++ loopy.js
@@ -9,7 +9,7 @@ for (let loop_n = 100; loop_n <= 200; loop_n += 1 ) {
   if ( loop_n % 3 === 0 && loop_n %4 === 0 ) {
     console.log(LoopyLighthouse);
   }
-  if (loop_n % 3 != 0 || loop_n %4 != 0) {
+  if (loop_n % 3 != 0 && loop_n %4 != 0) {
     console.log (loop_n);
   }
 }
