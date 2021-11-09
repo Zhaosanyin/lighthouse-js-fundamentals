7d910542de54d2d9a121c3990706c4d19f08619f
zhaosanyin <zhaosan.yin@gmail.com>
2021-11-09 22:08:14 +0000

    hello.js

--- hello.js
+++ hello.js
@@ -1,3 +1,13 @@
 const sayHello = function (name){
 	console.log("Hello," + name);
 }
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John11'); 
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('Joh11n');
+console.log(greeting);
+
