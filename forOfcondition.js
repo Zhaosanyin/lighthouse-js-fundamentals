1ef6c17ccafac7a78ad2ba2914c4aabe30e9fabe
zhaosanyin <zhaosan.yin@gmail.com>
2023-01-18 00:36:04 +0000

    newer loop in js

---
+++ forOfcondition.js
@@ -0,0 +1,14 @@
+/*
+const amount = [61.00, 52.25, 112.99, 5.00];
+let total = 0;
+for (let i = 0; i < amounts.length; i++) {
+  total +=amount[i];
+}
+console.log("Order total is :", total);
+*/
+let amount = [1, 2, 3, 4];
+let total = 0;
+for (let a1 of amount) {
+  total += a1;
+}
+console.log('Order total is:', total);
