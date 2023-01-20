c4945b46788715f33f0b85d5dc7805d8e94f8326
zhaosanyin <zhaosan.yin@gmail.com>
2023-01-20 19:43:06 +0000

    string return useless

---
+++ functionIsThisWorking.js
@@ -0,0 +1,9 @@
+function isThisWorking(input) {
+  console.log("Printing: isThisWorking was called and " + input + " was passed in as an argument.");
+  return "Returning: I am returning this string!";
+}
+
+isThisWorking(3);
+
+
+// return " " 这个return的string 好像没有作用啊。
