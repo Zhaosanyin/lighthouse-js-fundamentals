1278401267f04e4f7b5a3665f85a19d60e2efeaa
zhaosanyin <zhaosan.yin@gmail.com>
2023-01-20 19:49:23 +0000

    string display and return function

---
+++ functionsleep.js
@@ -0,0 +1,10 @@
+function sleep() {
+  console.log("I'm sleepy!");
+  return "zzz";
+  return "snore";
+}
+
+sleep();
+
+
+// this sleep() will display "I'm sleepy!", but the return function is "zzz" instead of "snore".
